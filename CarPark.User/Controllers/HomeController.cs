3ccb08e4fb3f65d28e61e09a210c4ac4a743ac46
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2021-05-06 00:14:33 +0300

    Database Test

---
+++ CarPark.User/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using CarPark.User.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarPark.User.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var client = new MongoClient("mongodb+srv://enesayyildiz:yozgatordu@carparkcluster.bmiv4.mongodb.net/CarParkDB?retryWrites=true&w=majority");
+            var database = client.GetDatabase("CarParkDB");
+            var collection = database.GetCollection<Test>("Test");
+
+            var test = new Test()
+            {
+                _Id = ObjectId.GenerateNewId(),
+                NameSurname = "Enes Ayyıldız",
+                Age = 19,
+                AddressList = new List<Address>()
+                {
+                    new Address
+                {
+                    Title="Ev Adresim",
+                    Description="Zeytinburnu"
+                    },
+                       new Address
+                {
+                    Title="İş Adresim",
+                    Description="Levent 4"
+                }
+
+                }
+
+            };
+            collection.InsertOne(test);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
